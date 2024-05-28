937cecbddb656bca7095e55e118511e52d993b9c
S283956 <s283956@studenti.polito.it>
2024-05-28 13:20:57 +0200

    prova 2

--- model/model.py
+++ model/model.py
@@ -1,3 +1,18 @@
 class Model:
-    def __init__(self):
-        pass
+    @staticmethod
+    def getAllAirports():
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """SELECT * from airports a order by a.AIRPORT asc"""
+
+        cursor.execute(query)
+
+        for row in cursor:
+            result.append(Airport(**row))
+
+        cursor.close()
+        conn.close()
+        return result
